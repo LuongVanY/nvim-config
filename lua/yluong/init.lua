c7df1ebb913e68a64d64b3bcdc5345d7f50c462f
MotCaiBanhBao <56585902+MotCaiBanhBao@users.noreply.github.com>
2023-09-24 23:06:55 +0700

    first commit

---
+++ lua/yluong/init.lua
@@ -0,0 +1,6 @@
+require("yluong.options")
+require("yluong.commands")
+require("yluong.lazy")
+require("yluong.remaps")
+
+-- vim.cmd([[colorscheme rose-pine]]) -- some issues with gitgutter color if in after
